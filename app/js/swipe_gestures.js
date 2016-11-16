2ee7ba29c4282b23a30440cebf3b27448f9336f7
wangkai1995 <wwwwkwy@126.com>
2016-11-16 19:06:44 +0800

    开发文件

--- app/js/swipe_gestures.js
+++ app/js/swipe_gestures.js
@@ -1,10 +1,39 @@
 
-
-
 window.onload = function(){
-    console.log(1111111);
 
+    var swipe = Swipe('.swipe-container');
+
+    console.log(swipe);
 
 };
 
 
+function Swipe(select){
+    var swipe = {};
+
+    var container = {
+            height: 0,
+            width: 0
+        },
+        item = '';
+
+    var startX = 0,
+        startY = 0,
+        startTime  = 0;
+
+
+    function init(select){
+        var war = document.querySelectorAll(select)[0];
+        container.height = war.clientHeight;
+        container.width = war.clientWidth
+        item = document.querySelectorAll(select+' div');
+
+        console.log(container);
+        console.log(item);
+    };
+
+    init(select);
+
+    return swipe;
+  
+}
