1ae8360b437320f453da1a45df02e90fbca6081d
wangkai1995 <wwwwkwy@126.com>
2016-11-19 16:44:32 +0800

    开发中

--- dist/js/all.js
+++ dist/js/all.js
@@ -44,7 +44,7 @@ function Swipe(config){
     /***************公用函数部分******************/  
 
     //按住不松开的滑动处理
-    //暂未完善，有待修改
+    //暂未完善，有待修改 上下滑动未处理
     function pressSwipe(clientX,clientY){
         //这种方式是及时移动 在安卓下面体验不太好 尝试另外一种 在touchEndEvent 中做处理
         //如果是左右滑动
@@ -105,8 +105,36 @@ function Swipe(config){
         }
     }  
 
-    //快速滑动
+    //快速滑动 滑动距离=移动距离X2
+    //上下滑动未处理
     function quickSlide(){
+        //判断上下滑动还是左右滑动
+        var moveX  = end.x - start.x,
+            moveY = end.y - start.y;
+
+            //左右滑动
+            if( Math.abs(moveX) > Math.abs(moveY) ){   
+                //右滑动距离限制
+
+                container.moveLeft  += moveX*2;
+
+                if(container.moveLeft > 0){
+                    container.moveLeft = 0;
+                //左滑动距离限制
+                }else if( (Math.abs(container.moveLeft) + viem.totalWidth) > item.totalWidth ){
+                    container.moveLeft = -(item.totalWidth-viem.totalWidth);
+                }
+
+                //这里还需要修改 IOS9.0以下和安卓5 以下需要加webkit 以上则不需要加
+                container.dom.setAttribute('style','-webkit-transform: translate3d('+container.moveLeft+'px, 0px, 0px);');
+            }else{
+                //上下滑动
+            }
+    }
+
+    //慢速滑动 滑动距离=移动距离X1
+    //上下滑动未处理
+    function slowSlide(){
         //判断上下滑动还是左右滑动
         var moveX  = end.x - start.x,
             moveY = end.y - start.y;
@@ -124,18 +152,11 @@ function Swipe(config){
                     container.moveLeft = -(item.totalWidth-viem.totalWidth);
                 }
 
-                console.log(container.moveLeft );
-
                 //这里还需要修改 IOS9.0以下和安卓5 以下需要加webkit 以上则不需要加
                 container.dom.setAttribute('style','-webkit-transform: translate3d('+container.moveLeft+'px, 0px, 0px);');
             }else{
                 //上下滑动
-
             }
-    }
-
-    //慢速滑动
-    function slowSlide(){
     }
 
     /*****************初始化**********************/    
@@ -165,7 +186,8 @@ function Swipe(config){
     }
     //滑动开始事件
     function touchStartEvent(event){
-        event.stopPropagation();
+        event.stopPropagation();          //阻止冒泡传播
+        event.preventDefault();           //阻止默认行为 这个很重要 在有滑动翻页的浏览器下 不禁止这个行为会导致翻页
         start.x = event.changedTouches[0].clientX;
         start.y = event.changedTouches[0].clientY;
         start.time = new Date().getTime();
@@ -174,21 +196,22 @@ function Swipe(config){
     }
     //滑动中事件
     function touchMoveEvent(event){
-        event.stopPropagation();
+        event.stopPropagation();     //阻止冒泡传播
+        event.preventDefault();      //阻止默认行为 这个很重要 在有滑动翻页的浏览器下 不禁止这个行为会导致翻页
         var clientX = event.changedTouches[0].clientX,
             clientY = event.changedTouches[0].clientY;
 
-        //这种方式是按住不放移动 在安卓下面体验不太好 尝试另外一种 在touchEndEvent 中做处理
-        // pressSwipe(clientX,clientY);
-
+        //这种方式是按住不放移动
+        pressSwipe(clientX,clientY);
     }
     //滑动结束事件
     function touchEndEvent(event){
-        event.stopPropagation();
+        event.stopPropagation();      //阻止冒泡传播
+        event.preventDefault();       //阻止默认行为 这个很重要 在有滑动翻页的浏览器下 不禁止这个行为会导致翻页
         end.x = event.changedTouches[0].clientX;
         end.y = event.changedTouches[0].clientY;
         end.time = new Date().getTime();
-
+        //松手之后
         letGoSwipe();
     }
 
