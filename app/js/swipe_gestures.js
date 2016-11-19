00ab5f1d8be83d742dbd5fbb8e17898ec883bf39
wangkai1995 <wwwwkwy@126.com>
2016-11-19 12:16:38 +0800

    开发中

--- app/js/swipe_gestures.js
+++ app/js/swipe_gestures.js
@@ -41,61 +41,10 @@ function Swipe(config){
             time : 0
         };
 
-    /***************公用函数部分******************/    
+    /***************公用函数部分******************/  
 
-
-    /*****************初始化**********************/    
-    function init(select){
-        container.war = document.querySelectorAll('.swipe-war')[0];
-        container.dom = document.querySelectorAll('.swipe-container')[0];
-        container.height = container.dom.clientHeight;
-        container.width = container.dom.clientWidth;
-        item.dom= document.querySelectorAll('.swipe-container .swipe-item');
-        for(var i=0,len =item.dom.length; i<len ;i++){
-            item.totalWidth += item.dom[i].clientWidth;
-        }
-
-        console.log(container);
-        console.log(item);
-        console.log(viem);
-
-        touchEventInit();
-    }
-
-    /******************滑动事件*******************/
-    //注册滑动事件
-    function touchEventInit(){
-        container.war.addEventListener('touchstart',touchStartEvent,false);
-        container.war.addEventListener('touchmove',touchMoveEvent,false);
-        container.war.addEventListener('touchend',touchEndEvent,false);
-    }
-    //滑动开始事件
-    function touchStartEvent(event){
-        event.stopPropagation();
-        start.x = event.changedTouches[0].clientX;
-        start.y = event.changedTouches[0].clientY;
-        start.time = new Date().getTime();
-        move.x = 0;
-        move.y = 0;
-    }
-    //滑动中事件
-    function touchMoveEvent(event){
-        event.stopPropagation();
-        var clientX = event.changedTouches[0].clientX,
-            clientY = event.changedTouches[0].clientY;
-
-            //这种方式是及时移动 在安卓下面体验不太好 尝试另外一种 在touchEndEvent 中做处理
-            pressSwipe(clientX,clientY);
-    }
-    //滑动结束事件
-    function touchEndEvent(event){
-        event.stopPropagation();
-        end.x = event.changedTouches[0].clientX;
-        end.y = event.changedTouches[0].clientY;
-        end.time = new Date().getTime();
-    }
-
-    /************按住不松开的滑动处理**************/
+    //按住不松开的滑动处理
+    //暂未完善，有待修改
     function pressSwipe(clientX,clientY){
         //这种方式是及时移动 在安卓下面体验不太好 尝试另外一种 在touchEndEvent 中做处理
         //如果是左右滑动
@@ -143,10 +92,108 @@ function Swipe(config){
         }
     }
 
-    /************松手之后的滑动处理****************/
+    //松手之后的滑动处理
     function letGoSwipe(){
+        //判断快速滑动还是慢速滑动
+        //@阀值设为500MS
+        var moveTime = end.time - start.time;
+
+        if(moveTime < 500){
+            quickSlide();
+        }else{
+            slowSlide();
+        }
+    }  
+
+    //快速滑动
+    function quickSlide(){
+        //判断上下滑动还是左右滑动
+        var moveX  = end.x - start.x,
+            moveY = end.y - start.y;
+
+            //左右滑动
+            if( Math.abs(moveX) > Math.abs(moveY) ){   
+                //右滑动距离限制
+
+                container.moveLeft  += moveX;
+
+                if(container.moveLeft > 0){
+                    container.moveLeft = 0;
+                //左滑动距离限制
+                }else if( (Math.abs(container.moveLeft) + viem.totalWidth) > item.totalWidth ){
+                    container.moveLeft = -(item.totalWidth-viem.totalWidth);
+                }
+
+                console.log(container.moveLeft );
+
+                //这里还需要修改 IOS9.0以下和安卓5 以下需要加webkit 以上则不需要加
+                container.dom.setAttribute('style','-webkit-transform: translate3d('+container.moveLeft+'px, 0px, 0px)');
+            }else{
+                //上下滑动
+
+            }
+    }
+
+    //慢速滑动
+    function slowSlide(){
+    }
+
+    /*****************初始化**********************/    
+    function init(select){
+        container.war = document.querySelectorAll('.swipe-war')[0];
+        container.dom = document.querySelectorAll('.swipe-container')[0];
+        container.height = container.dom.clientHeight;
+        container.width = container.dom.clientWidth;
+        item.dom= document.querySelectorAll('.swipe-container .swipe-item');
+        for(var i=0,len =item.dom.length; i<len ;i++){
+            item.totalWidth += item.dom[i].clientWidth;
+        }
+
+        console.log(container);
+        console.log(item);
+        console.log(viem);
+
+        touchEventInit();
+    }
+
+    /******************滑动事件*******************/
+    //注册滑动事件
+    function touchEventInit(){
+        container.war.addEventListener('touchstart',touchStartEvent,false);
+        container.war.addEventListener('touchmove',touchMoveEvent,false);
+        container.war.addEventListener('touchend',touchEndEvent,false);
+    }
+    //滑动开始事件
+    function touchStartEvent(event){
+        event.stopPropagation();
+        start.x = event.changedTouches[0].clientX;
+        start.y = event.changedTouches[0].clientY;
+        start.time = new Date().getTime();
+        move.x = 0;
+        move.y = 0;
+    }
+    //滑动中事件
+    function touchMoveEvent(event){
+        event.stopPropagation();
+        var clientX = event.changedTouches[0].clientX,
+            clientY = event.changedTouches[0].clientY;
+
+        //这种方式是按住不放移动 在安卓下面体验不太好 尝试另外一种 在touchEndEvent 中做处理
+        // pressSwipe(clientX,clientY);
 
     }
+    //滑动结束事件
+    function touchEndEvent(event){
+        event.stopPropagation();
+        end.x = event.changedTouches[0].clientX;
+        end.y = event.changedTouches[0].clientY;
+        end.time = new Date().getTime();
+
+        letGoSwipe();
+    }
+
+
+    /******************初始化运行*******************/
 
     init();
 
